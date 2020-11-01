9ac5258f9d80ef9363e0b7a66141ecec0bf28340
Gonzalo Iván López Acosta <ivan.lopez.5737@gmail.com>
2020-10-31 23:43:05 -0600

    Se da style a codigo java

--- src/main/java/com/jasper/model/Tarjetas.java
+++ src/main/java/com/jasper/model/Tarjetas.java
@@ -2,69 +2,70 @@ package com.jasper.model;
 
 public class Tarjetas {
 
-	private Integer numero;
+  private Integer numero;
 
-	private String producto;
+  private String producto;
 
-	private String folio;
+  private String folio;
 
-	private Double importe;
+  private Double importe;
 
-	private String comprobante;
+  private String comprobante;
 
-	public Tarjetas(Integer numero, String producto, String folio, Double importe, String comprobante) {
-		super();
-		this.numero = numero;
-		this.producto = producto;
-		this.folio = folio;
-		this.importe = importe;
-		this.comprobante = comprobante;
-	}
+  public Tarjetas(Integer numero, String producto, String folio, Double importe,
+      String comprobante) {
+    super();
+    this.numero = numero;
+    this.producto = producto;
+    this.folio = folio;
+    this.importe = importe;
+    this.comprobante = comprobante;
+  }
 
-	public Integer getNumero() {
-		return numero;
-	}
+  public Integer getNumero() {
+    return numero;
+  }
 
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
 
-	public String getProducto() {
-		return producto;
-	}
+  public String getProducto() {
+    return producto;
+  }
 
-	public void setProducto(String producto) {
-		this.producto = producto;
-	}
+  public void setProducto(String producto) {
+    this.producto = producto;
+  }
 
-	public String getFolio() {
-		return folio;
-	}
+  public String getFolio() {
+    return folio;
+  }
 
-	public void setFolio(String folio) {
-		this.folio = folio;
-	}
+  public void setFolio(String folio) {
+    this.folio = folio;
+  }
 
-	public Double getImporte() {
-		return importe;
-	}
+  public Double getImporte() {
+    return importe;
+  }
 
-	public void setImporte(Double importe) {
-		this.importe = importe;
-	}
+  public void setImporte(Double importe) {
+    this.importe = importe;
+  }
 
-	public String getComprobante() {
-		return comprobante;
-	}
+  public String getComprobante() {
+    return comprobante;
+  }
 
-	public void setComprobante(String comprobante) {
-		this.comprobante = comprobante;
-	}
+  public void setComprobante(String comprobante) {
+    this.comprobante = comprobante;
+  }
 
-	@Override
-	public String toString() {
-		return "Tarjetas [numero=" + numero + ", producto=" + producto + ", folio=" + folio + ", importe=" + importe
-				+ ", comprobante=" + comprobante + "]";
-	}
+  @Override
+  public String toString() {
+    return "Tarjetas [numero=" + numero + ", producto=" + producto + ", folio=" + folio
+        + ", importe=" + importe + ", comprobante=" + comprobante + "]";
+  }
 
 }
